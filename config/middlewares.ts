c59063e393e25e1dfee3f10dba5c615126e9fc23
balaprasad-pdai <bala@populardigital.ai>
2025-11-17 20:00:02 +0530

    fix things

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,33 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "*.digitaloceanspaces.com", // <= Allow all spaces and CDN
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "*.digitaloceanspaces.com",
+          ],
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
